b8525a7138024540237ac1f6614b78c00e729682
coldboya123 <65642613+coldboya123@users.noreply.github.com>
2020-06-24 18:47:21 +0700

    Revert "24/06"
    This reverts commit c8cd9957c113b14b5459b1398c9a624fad51f52c.

---
+++ doan/src/app/teacher-class/teacher-class.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-class',
+  templateUrl: './teacher-class.page.html',
+  styleUrls: ['./teacher-class.page.scss'],
+})
+export class TeacherClassPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
